93421cb02c41db438a6109f6e0cbaf22741d88cf
Ivan <133674171+EvanQa@users.noreply.github.com>
2024-09-07 20:30:07 +0300

    Add files via upload

---
+++ HomeworkN3/Exercise1.py
@@ -0,0 +1,41 @@
+#1)
+id: str = str(input("Enter your id"))
+
+name: str = str(input("Enter your name"))
+
+height: float = float(input("Enter your height"))
+
+birth_y: int = int(input("Enter your year of birth"))
+
+print(f"id: {id} name: {name} height: {height} year of birth is: {birth_y}")
+
+#2)
+
+# id1: int = 38949185
+#
+# name1: str = "Ivan Khristoforov"
+# #             12345678901234567
+# height1: float = 1.85
+#
+# birth_y1: int = 2001
+#
+#
+# id2: int = 31234910
+#
+# name2: str = "Shani Choen"
+# #             12345678901
+# height2: float = 1.72
+#
+# birth_y2: int = 1999
+#
+# id3: int = 33478282
+#
+# name3: str = "Chen Mizrahi"
+# #             123456789012
+# height3: float = 1.64
+#
+# birth_y3: int = 2005
+#
+# print(f"id: {id1} name: {name1:^17}, year of birth: {birth_y1}, height: {height1}")
+# print(f"id: {id2} name: {name2:<17}, year of birth: {birth_y2}, height: {height2}")
+# print(f"id: {id3} name: {name3:<17}, year of birth: {birth_y3}, height: {height3}")
